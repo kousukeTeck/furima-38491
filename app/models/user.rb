5a5e6013dec02aff3c90b07dbca0dbdeb4006756
kousukeTeck <kousuke1160@gmail.com>
2022-09-30 18:29:31 +0900

    ユーザー管理機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
-
+  #has_many :items
+  #has_many :orders
+  
   with_options presence: true do
-    validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/}
+
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
     validates :nickname
-    validates :email,    uniqueness: true
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
