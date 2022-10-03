91d6055cd81a7d2497aa2e5c7d94795316faf7e3
kousukeTeck <kousuke1160@gmail.com>
2022-10-03 16:39:49 +0900

    導入・バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,13 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :cost_id, :prefecture_id, :shopping_date_id, :price).merge(user_id: current_user.id)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,26 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  #belongs_to:user
+  #has_one:order
+  has_one_attached :image
+
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :shopping_date_id
+  end
+end
