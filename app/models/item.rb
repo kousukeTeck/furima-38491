8092309d86db74791d5f2a3c786dad3f18f41244
kousukeTeck <kousuke1160@gmail.com>
2022-10-03 18:38:40 +0900

    カテゴリー選択のモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,8 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :prefecture
   belongs_to :item_status
-  belongs_to :shipping_cost
-  belongs_to :shipping_date
+  belongs_to :cost
+  belongs_to :shopping_date
 
   with_options presence: true do
     validates :user_id
@@ -21,6 +21,14 @@ class Item < ApplicationRecord
     validates :cost_id
     validates :prefecture_id
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :shopping_date_id
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :status_id
+    validates :cost_id
     validates :shopping_date_id
   end
 end
